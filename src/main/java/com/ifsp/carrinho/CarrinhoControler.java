6515aa76db7410ad71ad6ed0ae75acbefdd61f72
3036308 <3036308@itp.local@LABFMAQ05.itp.local>
2026-04-13 08:51:45 -0300

    ex 1 + css basico

---
+++ src/main/java/com/ifsp/carrinho/CarrinhoControler.java
@@ -0,0 +1,37 @@
+package com.ifsp.carrinho;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CarrinhoControler {
+    private List<Produto> produtos = new ArrayList<>();
+    
+    @GetMapping("/formCarrinho")
+    public String formCarrinho() {
+
+        return "formCarrinho.html";
+    }
+
+    @PostMapping("/cadastroCarrinho") 
+    public String cadastroCarrinhho(@RequestParam int id, @RequestParam String nome, @RequestParam double preco, @RequestParam int quantidade) {
+        
+        produtos.add(new Produto(id, nome, preco, quantidade));
+        return "redirect:/carrinho";
+    }
+
+    @GetMapping("/carrinho")
+    public String carrinho(Model model) {
+        boolean vazio = produtos.isEmpty();;
+        
+        model.addAttribute("vazio", vazio);
+        model.addAttribute("produtos", produtos);
+        return "carrinho.html";
+    }
+}
