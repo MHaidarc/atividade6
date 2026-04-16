1226c91cd016cc3bc2527ec619b34ee2b7074650
maty haidar <mateus.gcarvalho@proton.me>
2026-04-16 14:54:10 -0300

    etapa 1 completa

--- src/main/java/com/ifsp/carrinho/CarrinhoControler.java
+++ src/main/java/com/ifsp/carrinho/CarrinhoControler.java
@@ -12,24 +12,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class CarrinhoControler {
     private List<Produto> produtos = new ArrayList<>();
-    
+
     @GetMapping("/formCarrinho")
     public String formCarrinho() {
 
         return "formCarrinho.html";
     }
 
-    @PostMapping("/cadastroCarrinho") 
-    public String cadastroCarrinhho(@RequestParam int id, @RequestParam String nome, @RequestParam double preco, @RequestParam int quantidade) {
-        
+    @PostMapping("/cadastroCarrinho")
+    public String cadastroCarrinhho(@RequestParam int id, @RequestParam String nome, @RequestParam double preco,
+            @RequestParam int quantidade) {
+
         produtos.add(new Produto(id, nome, preco, quantidade));
         return "redirect:/carrinho";
     }
 
     @GetMapping("/carrinho")
     public String carrinho(Model model) {
-        boolean vazio = produtos.isEmpty();;
-        
+        boolean vazio = produtos.isEmpty();
+        int precoTotal = 0;
+        int quantTotal = 0;
+
+        for (Produto produto : produtos) {
+            precoTotal += produto.getPreco() * produto.getQuantidade();
+            quantTotal += produto.getQuantidade();
+        }
+
+        model.addAttribute("quantTotal", quantTotal);
+        model.addAttribute("precoTotal", precoTotal);
         model.addAttribute("vazio", vazio);
         model.addAttribute("produtos", produtos);
         return "carrinho.html";
